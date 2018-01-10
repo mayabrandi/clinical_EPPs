019e93362b620f7f69e9fbc531765dc1b5d1636a
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2018-01-10 15:18:02 +0100

    New bravo file script

---
+++ EPPs/make_bravo_csv.py
@@ -0,0 +1,78 @@
+#!/home/glsai/miniconda2/envs/epp_master/bin/python
+from __future__ import division
+from argparse import ArgumentParser
+
+from genologics.lims import Lims
+from genologics.config import BASEURI,USERNAME,PASSWORD
+
+from genologics.entities import Process
+from genologics.epp import EppLogger
+
+import logging
+import sys
+import csv
+
+
+DESC = """epp script to generate Bravo CSV file
+Written by Maya Brandi, Science for Life Laboratory, Stockholm, Sweden
+"""
+
+
+class BravoCSV():
+
+    def __init__(self, process, bravo_csv):
+        self.csv = bravo_csv+'_bravo_nomalization.csv'
+        self.process = process
+        self.artifacts = []
+        self.failed_arts = []
+
+    def get_artifacts(self):
+        all_artifacts = self.process.all_outputs(unique=True)
+        self.artifacts = filter(lambda a: a.output_type == "Analyte" , all_artifacts)
+
+    def make_csv(self):
+        with open( self.csv , 'wb') as bravo_csv:
+            wr = csv.writer(bravo_csv)
+            art_list = [(a.location[1][2], a.location[1][0] , a) for a in self.artifacts]
+            art_list.sort() 
+            for col, row, art  in art_list:
+                well = art.location[1].replace(':','')
+                try:
+                    row_list = [well, str(art.udf['Volume Buffer (ul)']), str(art.udf['Sample Volume (ul)'])]
+                    wr.writerow(row_list)
+                except:
+                    self.failed_arts.append(art.name)
+
+def main(lims, args):
+    process = Process(lims, id = args.pid)
+    BCSV = BravoCSV(process, args.csv)
+    BCSV.get_artifacts()
+    BCSV.make_csv()
+
+
+    d = {'ia': ', '.join(BCSV.failed_arts)}
+
+    if BCSV.failed_arts:
+        abstract = ("Wrong and/or blank values for some udfs. Could not add sample "
+                 "information to Bravo CSV for the following samples: {ia}").format(**d)
+        sys.exit(abstract)
+    else:
+        abstract = "Bravo CSV sucsessfully generated."
+        print >> sys.stderr, abstract
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=DESC)
+    parser.add_argument('--pid',
+                        help='Lims id for current Process')
+    parser.add_argument('--log', default=sys.stdout,
+                        help=('File name for standard log file, '
+                              'for runtime information and problems.'))
+    parser.add_argument('--csv', default=sys.stdout,
+                        help=('csv file'))
+
+    args = parser.parse_args()
+
+    lims = Lims(BASEURI, USERNAME, PASSWORD)
+    lims.check_version()
+    main(lims, args)
