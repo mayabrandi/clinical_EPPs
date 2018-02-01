12ae4612c6444420af38db7b83869ceb58b9984f
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2018-02-01 11:28:30 +0100

    Added handeling of outlyers

--- EPPs/qPCR_dilution.py
+++ EPPs/qPCR_dilution.py
@@ -38,17 +38,15 @@ class QpcrDilution():
         self.failed_arts = 0
         self.dilution_data = {}
         self.removed_replicates = 0
+        self.failed_samples = 0
 
     def get_artifacts(self):
         in_arts = self.process.all_inputs(unique=True)
         all_artifacts = self.process.all_outputs(unique=True)
         out_artifacts = filter(lambda a: a.output_type == "ResultFile" , all_artifacts)
-        for a in in_arts:
-            samp = a.samples[0].id
-            self.artifacts[samp] = {'in': a}
         for a in out_artifacts:
             samp = a.samples[0].id
-            self.artifacts[samp]['out'] = a
+            self.artifacts[samp] = a
 
     def get_file(self):
         try:
@@ -61,6 +59,10 @@ class QpcrDilution():
 
 
     def make_dilution_data(self, dilution_file):
+        """
+        Reads the qPCR dilution Resultfile. 
+        Uses WELL_TRANSFORMER to conect each well in the file to its original well.
+        Stores the data from the qPCR file in a dict with original well as keyes."""
         dilution_file = self.get_file()
         df = pd.read_excel(dilution_file)
         for self.index, row in df.iterrows():
@@ -78,7 +80,10 @@ class QpcrDilution():
                     self.dilution_data[orwell]['SQ'][dilut].append(SQ)
                     self.dilution_data[orwell]['Cq'][dilut].append(Cq)
 
-    def set_udf(self):
+    def set_all_samples(self):
+        """ For each sample:
+            Checks dilution thresholds as described in am doc 1499.
+            Calculates concentration and sets udfs for those samples that passed the check. """
         for samp_id, art in self.artifacts.items():
             self.log.write('\n############################################\n')
             self.log.write('Sample: ' + samp_id + '\n')
@@ -86,132 +91,126 @@ class QpcrDilution():
             PA.check_dilution_range()
             PA.check_distance_find_outlyer()
             for dil in PA.index.keys():
-                self.log.write(dil + ' Measurements : ' + str(PA.Cq[dil])+'\n')
-                if PA.index[dil] is not None:
+                ind = PA.index[dil]
+                if type(ind)==int:
                     self.removed_replicates += 1
-                    self.log.write('Removed measurement: ' + str(PA.poped_dilutes[dil]) + '\n')
-            passed = PA.calculate_molar()
-            if passed:
-                self.passed_arts +=1
+                if ind != 'Fail':
+                    self.log.write(dil + ' Measurements : ' + str(PA.Cq[dil])+'\n')
+                    if PA.poped_dilutes[dil]:
+                        self.log.write('Removed measurement: ' + str(PA.poped_dilutes[dil]) + '\n')
+            if PA.failed_sample:
+                self.failed_samples +=1
             else:
-                self.failed_arts +=1
+                passed = PA.set_udfs()
+                if passed:
+                    self.passed_arts +=1
+                else:
+                    self.failed_arts +=1
 
 
 class PerArtifact():
-    """Artifact specific class do determine messuement outlyers and calculate dilution"""
+    """Artifact specific class do determine messuement outlyers and calculate dilution.
+    CHECK 1.
+        Compares the Cq-values within each dilution for the sample.
+        If they differ by more than 0.4 within the dilution:
+            a)  The the Cq-value differing moste from the mean, is removed and 
+                its index is stored in self.index.
+            b)  If the two remaining Cq-values still differ by more than 0.4,
+                The self.failed_sample is set to true
+    CHECK 2.
+        Compares the Cq-values between the different dilution series; 1E03, 2E03 and 1E04:
+        If 0.7 >mean(2E03)-mean(1E03)> 1.5:
+            The bigest outlyer from the two series are compared and the bigest one, removed:
+                max(max(|mean(1E03)-1E03|), max(|mean(2E03)-2E03|)) 
+                Its index is stored in self.index
+        If 2.5 >mean(1E04)-mean(1E03)> 5:
+            The bigest outlyer from the two series are compared and the bigest one, removed:
+                max(max(|mean(1E03)-1E03|), max(|mean(1E04)-1E04|)) 
+                Its index is stored in self.index
+        This is repeated untill 0.7 <mean(2E03)-mean(1E03)< 1.5 and 2.5 <mean(1E04)-mean(1E03)< 5,
+        or untill a dilution series contains only one value. If this hapens, self.failed_sample is set to true."""
     def __init__(self, artifact, dilution_data, sample_id, log):
         self.log = log
         self.sample_id = sample_id
         self.dilution_data = dilution_data
         self.artifact = artifact
         self.size_bp = 470
-        self.outart = artifact['out']
+        self.outart = artifact
         self.well = self.outart.location[1]
         self.Cq = { '1E03' : self.dilution_data[self.well]['Cq']['1E03'], 
                     '2E03' : self.dilution_data[self.well]['Cq']['2E03'], 
                     '1E04' : self.dilution_data[self.well]['Cq']['1E04']}
         self.index = {'1E03' : None, '2E03' : None, '1E04' : None}
         self.poped_dilutes = {'1E03' : None, '2E03' : None, '1E04' : None}
-
+        self.failed_sample = False
 
     def check_dilution_range(self):
+        """CHECK 1.
+        Compares the Cq-values within each dilution for the sample.
+        If they differ by more than 0.4 within the dilution:
+            a)  The the Cq-value differing moste from the mean, is removed and 
+                its index is stored in self.index.
+            b)  If the two remaining Cq-values still differ by more than 0.4,
+                The self.failed_sample is set to true"""
         for dil, values in self.Cq.items():
             error_msg = 'To vide range of values for dilution: ' + dil + ' : ' + str(values)
             array = numpy.array(self.Cq[dil])   
             diff_from_mean = numpy.absolute(array - mean(array))
             while max(self.Cq[dil])-min(self.Cq[dil])> 0.4:
-                if self.index[dil] is not None:
-                    sys.exit(error_msg)
+                ind = self.index[dil]
+                if type(ind)==int:
+                    self.log.write(error_msg)
+                    self.failed_sample = True
+                    self.index[dil] = 'Fail'
+                    return 
                 else:
                     ind = numpy.argmax(diff_from_mean)
-                    self.index[dil] = ind
+                    self.index[dil] = int(ind)
                     self.poped_dilutes[dil] = self.Cq[dil].pop(ind)
-                    print self.poped_dilutes[dil]
                     array = numpy.array(self.Cq[dil])
                     diff_from_mean = numpy.absolute(array - mean(array))
-                    
+
 
     def check_distance_find_outlyer(self):
-        D1_in_range, D2_in_range = self.check_distance()
+        """CHECK 2.
+        Compares the Cq-values between the different dilution series; 1E03, 2E03 and 1E04:
+
+        If 0.7 >mean(2E03)-mean(1E03)> 1.5:
+            The bigest outlyer from the two series are compared and the bigest one, removed:
+                max(max(|mean(1E03)-1E03|), max(|mean(2E03)-2E03|)) 
+                Its index is stored in self.index
+
+        If 2.5 >mean(1E04)-mean(1E03)> 5:
+            The bigest outlyer from the two series are compared and the bigest one, removed:
+                max(max(|mean(1E03)-1E03|), max(|mean(1E04)-1E04|)) 
+                Its index is stored in self.index
+
+        This is repeated untill 0.7 <mean(2E03)-mean(1E03)< 1.5 and 2.5 <mean(1E04)-mean(1E03)< 5,
+        or untill a dilution series contains only one value. If this hapens, self.failed_sample is set to true."""
+
+        D1_in_range, D2_in_range = self._check_distance()
         while not (D1_in_range and D2_in_range):
-            self.find_outlyer(D1_in_range, D2_in_range)
+            self._find_outlyer(D1_in_range, D2_in_range)
+            if self.failed_sample:
+                return
             for dilute, ind in self.index.items():
-                if ind is not None:
+                if type(ind)==int and len(self.Cq[dilute])==3:
                     self.poped_dilutes[dilute] = self.Cq[dilute].pop(ind) 
-                    print 'kkkk'
-                    print self.poped_dilutes[dilute]
-            D1_in_range, D2_in_range = self.check_distance()            
+            D1_in_range, D2_in_range = self._check_distance()            
 
 
-    def check_distance(self):
-        """Compares the difference between the mean of the triplicates for the different dilutions. 
-        Checkes wether the differences are within accepted ranges"""
-        D1 = mean(self.Cq['1E04'])-mean(self.Cq['1E03'])
-        D1_in_range = 2.5 < D1 < 5
-        D2 = mean(self.Cq['2E03'])-mean(self.Cq['1E03'])   
-        D2_in_range = 0.7 < D2 < 1.5
-        return D1_in_range, D2_in_range
 
-    def find_outlyer(self, D1_in_range, D2_in_range):
-        control_1E03 = False
-        if not D1_in_range:
-            array = numpy.array(self.Cq['1E03'])
-            diff_from_mean_1E03 = numpy.absolute(array - mean(array))
-            outlyer_1E03 = max(diff_from_mean_1E03)
-            array = numpy.array(self.Cq['1E04'])
-            diff_from_mean_1E04 = numpy.absolute(array - mean(array)) 
-            outlyer_1E04 = max(diff_from_mean_1E04)
-            if outlyer_1E03 > outlyer_1E04:
-                if self.index['1E03'] is not None:
-                    self.log.write('1E03 Measurements : ' + str(self.Cq['1E03']) + '\n')
-                    self.log.write('Removed measurement: ' + str(self.poped_dilutes['1E03']) + '\n')
-                    self.log.write('One outlyer removed, but distance still to big. Skript excited.')
-                    sys.exit('Distance to big, tryed to remove outlyer but distance still not within range')
-                else:
-                    control_1E03 = True
-                    self.index['1E03'] = numpy.argmax(diff_from_mean_1E03)
-            else:
-                if self.index['1E04'] is not None:
-                    self.log.write('1E04 Measurements : ' + str(self.Cq['1E04']) + '\n')
-                    self.log.write('Removed measurement: ' + str(self.poped_dilutes['1E04']) + '\n')
-                    self.log.write('One outlyer removed, but distance still to big. Skript excited.')
-                    sys.exit('Distance to big, tryed to remove outlyer but distance still not within range')
-                else:
-                    self.index['1E04'] = numpy.argmax(diff_from_mean_1E04)
-        if not D2_in_range:
-            array = numpy.array(self.Cq['2E03'])
-            diff_from_mean_2E03 = numpy.absolute(array - numpy.median(array))/numpy.median(array).tolist()
-            outlyer_2E03 = max(diff_from_mean_2E03)
-            array = numpy.array(self.Cq['1E03'])
-            diff_from_mean_1E03 = numpy.absolute(array - numpy.median(array))/numpy.median(array).tolist()
-            outlyer_1E03 = max(diff_from_mean_1E03)
-            if outlyer_2E03 > outlyer_1E03:
-                if self.index['2E03'] is not None:
-                    self.log.write('2E03 Measurements : ' + str(self.Cq['2E03']) + '\n')
-                    self.log.write('Removed measurement: ' + str(self.poped_dilutes['2E03']) + '\n')
-                    self.log.write('One outlyer removed, but distance still to big. Skript excited.')
-                    sys.exit('Distance to big, tryed to remove outlyer but distance still not within range')
-                else:
-                    self.index['2E03'] = numpy.argmax(diff_from_mean_2E03)
-            else:
-                if self.index['1E03']:
-                    if control_1E03 and self.index['1E03'] != numpy.argmax(outlyer_1E03):
-                        sys.exit('Distance to big. Conflicting outlyers. ')
-                    elif not control_1E03:
-                        self.log.write('1E03 Measurements : ' + str(self.Cq['1E03']) + '\n')
-                        self.log.write('Removed measurement: ' + str(self.poped_dilutes['1E03']) + '\n')
-                        self.log.write('One outlyer removed, but distance still to big. Skript excited.')
-                        sys.exit('Distance to big, tryed to remove outlyer but distance still not within range')            
-                else:
-                    self.index['1E03'] = numpy.argmax(diff_from_mean_1E03)
-        return self.index
+    def set_udfs(self):
+        """ This will only happen if failed_sample is still False:
+        1. For every outlyer stored in self.index, removes its coresponding SQ messuement.
+        2. Calculates the zise adjusted concentraion based on the remaining SQ messuements.
+        3. Sets the artifact udfs; Concentration, Concentration (nM) and Size (bp)"""
 
-
-    def calculate_molar(self):
         for dilute, ind in self.index.items():
-            if ind is not None:
+            if type(ind)==int:
                 self.dilution_data[self.well]['SQ'][dilute].pop(ind)
                 # removing outlyer
+
         SQ_1E03 = mean(self.dilution_data[self.well]['SQ']['1E03'])
         SQ_2E03 = mean(self.dilution_data[self.well]['SQ']['2E03'])
         SQ_1E04 = mean(self.dilution_data[self.well]['SQ']['1E04'])
@@ -230,9 +229,96 @@ class PerArtifact():
             self.outart.put()
             return True
         except:
-            return False 
+            return False
 
-    
+
+    def _error_log_msg(self, dil):
+        """Log for failed sample"""
+        self.log.write(dil + ' Measurements : ' + str(self.Cq[dil]) + '\n')
+        self.log.write('Removed measurement: ' + str(self.poped_dilutes[dil]) + '\n')
+        self.log.write('One outlyer removed, but distance still to big. \n\n')
+
+
+    def _check_distance(self):
+        """Compares the difference between the mean of the triplicates for the different dilutions. 
+        Checkes wether the differences are within accepted ranges:
+        0.7 <mean(2E03)-mean(1E03)< 1.5 and 2.5 <mean(1E04)-mean(1E03)< 5"""
+        D1 = mean(self.Cq['1E04'])-mean(self.Cq['1E03'])
+        D1_in_range = 2.5 < D1 < 5
+        D2 = mean(self.Cq['2E03'])-mean(self.Cq['1E03'])   
+        D2_in_range = 0.7 < D2 < 1.5
+        return D1_in_range, D2_in_range
+
+    def _find_outlyer(self, D1_in_range, D2_in_range):
+        """        
+        If 0.7 >mean(2E03)-mean(1E03)> 1.5:
+            The bigest outlyer from the two series are compared and the bigest one, removed:
+                max(max(|mean(1E03)-1E03|), max(|mean(2E03)-2E03|)) 
+                Its index is stored in self.index
+
+        If 2.5 >mean(1E04)-mean(1E03)> 5:
+            The bigest outlyer from the two series are compared and the bigest one, removed:
+                max(max(|mean(1E03)-1E03|), max(|mean(1E04)-1E04|)) 
+                Its index is stored in self.index"""
+
+        control_1E03 = False
+        if not D1_in_range:
+            array = numpy.array(self.Cq['1E03'])
+            diff_from_mean_1E03 = numpy.absolute(array - mean(array))
+            outlyer_1E03 = max(diff_from_mean_1E03)
+            array = numpy.array(self.Cq['1E04'])
+            diff_from_mean_1E04 = numpy.absolute(array - mean(array)) 
+            outlyer_1E04 = max(diff_from_mean_1E04)
+            if outlyer_1E03 > outlyer_1E04:
+                ind = self.index['1E03']
+                if type(ind)==int:
+                    self._error_log_msg('1E03')
+                    self.failed_sample = True
+                    self.index['1E03'] = 'Fail'
+                    return 
+                else:
+                    control_1E03 = True
+                    self.index['1E03'] = int(numpy.argmax(diff_from_mean_1E03))
+            else:
+                ind = self.index['1E04']
+                if type(ind)==int:
+                    self._error_log_msg('1E04')
+                    self.failed_sample = True
+                    self.index['1E04'] = 'Fail'
+                    return 
+                else:
+                    self.index['1E04'] = int(numpy.argmax(diff_from_mean_1E04))
+        if not D2_in_range:
+            array = numpy.array(self.Cq['2E03'])
+            diff_from_mean_2E03 = numpy.absolute(array - numpy.median(array))/numpy.median(array).tolist()
+            outlyer_2E03 = max(diff_from_mean_2E03)
+            array = numpy.array(self.Cq['1E03'])
+            diff_from_mean_1E03 = numpy.absolute(array - numpy.median(array))/numpy.median(array).tolist()
+            outlyer_1E03 = max(diff_from_mean_1E03)
+            if outlyer_2E03 > outlyer_1E03:
+                ind = self.index['2E03']
+                if type(ind)==int:
+                    self._error_log_msg('2E03')
+                    self.failed_sample = True
+                    self.index['2E03'] = 'Fail'
+                    return 
+                else:
+                    self.index['2E03'] = int(numpy.argmax(diff_from_mean_2E03))
+            else:
+                if self.index['1E03']:
+                    if control_1E03 and self.index['1E03'] != numpy.argmax(outlyer_1E03):
+                        self.log.write('Distance to big. Conflicting outlyers. ')
+                        self.failed_sample = True
+                        self.index['1E03'] = 'Fail'
+                        return 
+                    elif not control_1E03:
+                        self._error_log_msg('1E03')
+                        self.failed_sample = True
+                        self.index['1E03'] = 'Fail'
+                        return 
+                else:
+                    self.index['1E03'] = int(numpy.argmax(diff_from_mean_1E03))
+
 
 def main(lims, args):
     log = open(args.log, 'a')
@@ -240,7 +326,7 @@ def main(lims, args):
     QD = QpcrDilution(process, log)
     QD.get_artifacts()
     QD.make_dilution_data(args.dil_file)
-    QD.set_udf()
+    QD.set_all_samples()
 
     d = {'ca': QD.passed_arts,
          'ia': QD.failed_arts}
@@ -250,9 +336,11 @@ def main(lims, args):
     if QD.removed_replicates:
         abstract += ' WARNING: Removed replicate from ' +str(QD.removed_replicates)+ ' samples. See log file for details.'  
 
+    if QD.failed_samples:
+        abstract += ' WARNING: Failed to set udfs on '+ str(QD.failed_samples)+' samples, due to unstable dilution messurements'
  
     log.close()
-    if QD.failed_arts:
+    if QD.failed_arts or QD.failed_samples:
         sys.exit(abstract)
     else:
         print >> sys.stderr, abstract
