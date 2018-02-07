c25e481ec3d9e689a684285518d0a66e956db754
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2018-02-07 16:28:44 +0100

    Make possible to generate two different types of bravo csv files

--- EPPs/make_bravo_csv.py
+++ EPPs/make_bravo_csv.py
@@ -20,8 +20,7 @@ Written by Maya Brandi, Science for Life Laboratory, Stockholm, Sweden
 
 class BravoCSV():
 
-    def __init__(self, process, bravo_csv):
-        self.csv = bravo_csv+'_bravo_nomalization.csv'
+    def __init__(self, process):
         self.process = process
         self.artifacts = []
         self.failed_arts = []
@@ -30,8 +29,9 @@ class BravoCSV():
         all_artifacts = self.process.all_outputs(unique=True)
         self.artifacts = filter(lambda a: a.output_type == "Analyte" , all_artifacts)
 
-    def make_csv(self):
-        with open( self.csv , 'wb') as bravo_csv:
+    def make_csv_1471(self, csv_file):
+        csv_file = csv_file+'bravo_normalization.csv'
+        with open( csv_file , 'wb') as bravo_csv:
             wr = csv.writer(bravo_csv)
             art_dict = {a.location[1] : a  for a in self.artifacts}
             for col in range(1,13):
@@ -45,12 +45,38 @@ class BravoCSV():
                         except:
                             self.failed_arts.append(art.name)
 
+
+    def make_csv_1564(self, udf, csv_file):
+        csv_file = csv_file+'_'+udf.replace(' ','')+'_bravo_normalization.csv'
+        with open( csv_file , 'wb') as bravo_csv:
+            wr = csv.writer(bravo_csv)
+            wr.writerow(['SourceBC','SourceWell','DestinationWell','Volume'])
+            art_dict = {a.location[1] : a  for a in self.artifacts}
+            for col in range(1,13):
+                for row in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
+                    well = row + ':' + str(col)
+                    if well in art_dict:
+                        art = art_dict[well]
+                        try:
+                            row_list = ['abc',row + str(col), row + str(col) ,  str(art.udf[udf])]
+                            wr.writerow(row_list)
+                        except:
+                            self.failed_arts.append(art.name)
+                    else:
+                        row_list = ['abc',row + str(col), row + str(col) ,  '0']
+                        wr.writerow(row_list)
+
+
+
 def main(lims, args):
     process = Process(lims, id = args.pid)
-    BCSV = BravoCSV(process, args.csv)
+    BCSV = BravoCSV(process)
     BCSV.get_artifacts()
-    BCSV.make_csv()
-
+    if args.doc1564:
+        BCSV.make_csv_1564('Sample Volume (ul)', args.sample_volume_file)
+        BCSV.make_csv_1564('Volume Buffer (ul)', args.buffer_volume_file)
+    if args.doc1471:
+        BCSV.make_csv_1471(args.csv)
 
     d = {'ia': ', '.join(BCSV.failed_arts)}
 
@@ -70,7 +96,15 @@ if __name__ == "__main__":
     parser.add_argument('--log', default=sys.stdout,
                         help=('File name for standard log file, '
                               'for runtime information and problems.'))
-    parser.add_argument('--csv', default=sys.stdout,
+    parser.add_argument('--csv', default=None,
+                        help=('csv file'))
+    parser.add_argument('--doc1471', action='store_true',
+                            help='Use this flagg to generate 1471 bravo file')
+    parser.add_argument('--doc1564', action='store_true',
+                            help='Use this flagg to generate 1564 bravo file')
+    parser.add_argument('--sample_volume_file', default=None,
+                        help=('csv file'))
+    parser.add_argument('--buffer_volume_file', default=None,
                         help=('csv file'))
 
     args = parser.parse_args()
