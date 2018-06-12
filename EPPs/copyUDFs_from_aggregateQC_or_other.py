f8053fdca395c962e9203fc07fc4a95c0628450c
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2018-06-12 09:45:09 +0200

    Adding possibility to search for udfs in different aggregate qc steps.

--- EPPs/copyUDFs_from_aggregateQC_or_other.py
+++ EPPs/copyUDFs_from_aggregateQC_or_other.py
@@ -12,18 +12,17 @@ import logging
 import sys
 
 
-DESC = """
-"""
+DESC = """"""
 
 
 class CopyUDF():
-    def __init__(self, process, udfs, aggregate_qc_step, ):
+    def __init__(self, process, udfs, aggregate_qc_steps ):
         self.process = process
         self.artifacts = []
         self.udfs = udfs
-        self.failded_udfs = '' 
+        self.failded_udfs = ''
         self.falied_processes = ''
-        self.aggregate_qc_step = aggregate_qc_step
+        self.aggregate_qc_steps = aggregate_qc_steps
 
     def get_artifacts(self):
         all_artifacts = self.process.all_outputs(unique=True)
@@ -34,30 +33,34 @@ class CopyUDF():
         child_processes = []
         parent_process = outpt.parent_process
         while parent_process:
-            parent_inputs = [a for a in parent_process.all_inputs() if a.type=='Analyte'] 
+            parent_inputs = [a for a in parent_process.all_inputs() if a.type=='Analyte']
             for parent_input in parent_inputs:
                 sample_names = [s.name  for s in parent_input.samples]
                 if sample in sample_names:
-                    child_processes =  lims.get_processes(type = self.aggregate_qc_step,
+                    for aggregate_qc_step in self.aggregate_qc_steps:
+                        child_processes =  lims.get_processes(type = aggregate_qc_step,
                                                 inputartifactlimsid = parent_input.id)
+                        if child_processes:
+                            break
                     if child_processes:
                         for udf in self.udfs:
                             try:
                                 outpt.udf[udf] = parent_input.udf[udf]
                                 outpt.put()
-                                logging.info('Sample {0}: Copied "Concentration (nM)" from output artifact of "{1}" to output artifact of current process'.format(sample, self.aggregate_qc_step))
+                                logging.info('Sample {0}: Copied "Concentration (nM)" from output artifact of "{1}" to output artifact of current process'.format(sample, aggregate_qc_step))
                             except:
                                 self.failded_udfs = 'Failed to copy some udfs.'
                         return True
                     else:
                         parent_process = parent_input.parent_process
-        return False 
+
+        return False
 
     def get_udfs_from_highpriostep(self, artifact, stop_process):
         sample = artifact.samples[0].name
         parent_process = artifact.parent_process
         while parent_process:
-            parent_inputs = [a for a in parent_process.all_inputs() if a.type=='Analyte']        
+            parent_inputs = [a for a in parent_process.all_inputs() if a.type=='Analyte']
             for parent_input in parent_inputs:
                 sample_names = [s.name  for s in parent_input.samples]
                 if sample in sample_names:
@@ -73,7 +76,7 @@ class CopyUDF():
                         parent_process = parent_input.parent_process
                     break
         return False
-                    
+
 
 
 def main(lims, args):
@@ -84,7 +87,7 @@ def main(lims, args):
         if args.priostep:
             if not CUDF.get_udfs_from_highpriostep(art, args.priostep):
                 if not CUDF.get_udfs_from_lowpriostep(art):
-                    CUDF.falied_processes = 'Some of the samples has not gone through any of the processes: '+args.qcstep+' or '+args.priostep + '. '
+                    CUDF.falied_processes = 'Some of the samples has not gone through any of the processes: ' + ', '.join(args.qcstep) + ' or '+args.priostep + '. '
 
     if CUDF.failded_udfs or CUDF.falied_processes:
         sys.exit(CUDF.falied_processes + CUDF.failded_udfs)
@@ -98,9 +101,9 @@ if __name__ == "__main__":
     parser.add_argument('-l', dest = 'log', default=sys.stdout,
                         help=('File name for standard log file, '
                               'for runtime information and problems.'))
-    parser.add_argument('-u', dest = 'udfs', nargs='+', 
+    parser.add_argument('-u', dest = 'udfs', nargs='+',
                         help=(''))
-    parser.add_argument('-q', dest = 'qcstep', 
+    parser.add_argument('-q', dest = 'qcstep', nargs='+',
                         help=(''))
     parser.add_argument('-r', dest = 'priostep', default = None,
                         help=('Use this option if there is a step with higher prio then the qc-step.'))
