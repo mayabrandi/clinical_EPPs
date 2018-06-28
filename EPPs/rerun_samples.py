2c9108072a2b64c4df1f9cdffa48435dac7785e6
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2018-06-28 15:17:39 +0200

    getting there

--- EPPs/rerun_samples.py
+++ EPPs/rerun_samples.py
@@ -30,37 +30,37 @@ class PassSamples():
 
     def __init__(self, process):
         self.process = process
-        self.out_arts = []
-        self.rerun_samples = {}
-        self.continue_arts = []
-        self.current_WF = self._get_current_WF() 
-        self.rerun_step =  ['CG002 - Sort HiSeq X Samples (HiSeq X)', 'CG002 - Sort HiSeq Samples']
-        self.rerun_stage = self._get_next_step_stage_URI(self.rerun_step)
-        self.continue_stage = self._get_next_step_stage_URI(['CG002 - Delivery'])
+        self.all_arts = self._get_all_arts()
+        self.rerun_arts = {}
+        self.arts_to_remove = []
+        self.current_WF = self._get_current_WF()
+        self.rerun_steps =  ['CG002 - Sort HiSeq X Samples (HiSeq X)', 'CG002 - Sort HiSeq Samples']
+        self.rerun_stage = self._get_next_step_stage_URI(self.rerun_steps)
         self.warning_duplicate_samples = []
-        self.rXML = []
-        self.abstract = ''
-        self.remove_from_WF = []
+        self.xml = []
 
-    def get_artifacts(self):
+    def _get_all_arts(self):
+        all_arts=[]
         for art in self.process.all_outputs(unique=True):
             if art.type == 'Analyte':
-                self.out_arts.append(art)
-                try:
-                    rerun = art.udf['Rerun']
-                except:
-                    rerun = False
-                if rerun:
-                    self._get_rerun_arts(art)
-                else:
-                    self.continue_arts.append(art)
+                all_arts.append(art)
+        return all_arts
+
+    def get_artifacts(self):
+        for art in self.all_arts:
+            try:
+                rerun = art.udf['Rerun']
+            except:
+                rerun = False
+            if rerun:
+                self._get_rerun_arts(art)
 
     def _get_rerun_arts(self, art):
         all_arts_in_sort=[]
         for sample in art.samples:
             sample_id = sample.id
             out_arts = lims.get_artifacts(samplelimsid = sample_id,
-                            process_type = self.rerun_step)
+                            process_type = self.rerun_steps)
             all_arts_in_sort += out_arts
         correct_process=None
         for art in all_arts_in_sort:
@@ -73,12 +73,11 @@ class PassSamples():
             if art.parent_process == correct_process:
                 art_sample_key = [s.id for s in art.samples]
                 art_sample_key = '_'.join(list(set(art_sample_key)))
-                self.rerun_samples[art_sample_key] = {'art' : art, 'next_step_stage' : self.rerun_stage}
-        print self.rerun_samples
+                self.rerun_arts[art_sample_key] = art
 
     def check_same_sample_in_many_rerun_pools(self):
         all_samples = []
-        for s in self.rerun_samples.keys():
+        for s in self.rerun_arts.keys():
             all_samples += s.split('_')
         for s in set(all_samples):
             all_samples.remove(s)
@@ -86,18 +85,14 @@ class PassSamples():
 
     def get_arts_to_remove(self):
         samples = []
-        for key, art in self.rerun_samples.items():
-            print 'hlkjhlkjhlkjhkl'
-            samples+=[s.id for s in art['art'].samples]
+        for key, art in self.rerun_arts.items():
+            samples+=[s.id for s in art.samples]
         samples=list(set(samples))
         for art in self.process.all_inputs():
             for samp in art.samples:
                 if samp.id in samples:
-                    print art
-                    self.remove_from_WF.append(art)
-        self.remove_from_WF=list(set(self.remove_from_WF))           
-        print self.remove_from_WF 
-
+                    self.arts_to_remove.append(art)
+        self.arts_to_remove=list(set(self.arts_to_remove))
 
     def _get_current_WF(self):
         art = self.process.all_inputs()[0]
@@ -106,38 +101,33 @@ class PassSamples():
         WF_node = art_dom.getElementsByTagName('workflow-stage')[-1]
         wf_id = WF_node.getAttribute('uri').split('/stages/')[0].split('/')[-1]
         try:
-            print 'WF'
-            print Workflow(lims,id = wf_id)
             return Workflow(lims,id = wf_id)
         except:
             sys.exit('Could not get the current Work Flow. Contact lims developer.')
 
     def _get_next_step_stage_URI(self, next_step):
         for stage in self.current_WF.stages:
-            print stage.name
             if stage.name in next_step:
                 return stage.uri
         sys.exit('Could not get the next step Stage. Contact lims developer.')
 
     def assign_arts(self):
-        for key, art in self.rerun_samples.items():
-            self.make_assig_xml(art['art'].uri, art['next_step_stage'])
-        for art in self.continue_arts:
-            self.make_assig_xml(art.uri, self.continue_stage)
+        for key, art in self.rerun_arts.items():
+            self.make_assig_xml(art.uri, self.rerun_stage)
 
     def make_assig_xml(self, art_uri, next_step_uri):
-        self.rXML.append( '<assign stage-uri="' + next_step_uri + '">' )
-        self.rXML.append( '<artifact uri="' + art_uri + '"/>' )
-        self.rXML.append( '</assign>' )
+        self.xml.append( '<assign stage-uri="' + next_step_uri + '">' )
+        self.xml.append( '<artifact uri="' + art_uri + '"/>' )
+        self.xml.append( '</assign>' )
 
     def remove_arts(self):
-        for art in self.remove_from_WF:
-            self.rXML.append( '<unassign workflow-uri="' + self.current_WF.uri + '">' )
-            self.rXML.append( '<artifact uri="' + art.uri + '"/>' )
-            self.rXML.append( '</unassign>' )
+        for art in self.arts_to_remove:
+            self.xml.append( '<unassign workflow-uri="' + self.current_WF.uri + '">' )
+            self.xml.append( '<artifact uri="' + art.uri + '"/>' )
+            self.xml.append( '</unassign>' )
 
     def rout(self):
-        routeXML = "".join( self.rXML )
+        routeXML = "".join( self.xml )
         if len(routeXML) > 0:
             routeXML = '<rt:routing xmlns:rt="http://genologics.com/ri/routing">' + routeXML + '</rt:routing>'
             responseXML = api.POST( routeXML, APIURI + "route/artifacts/" )
@@ -154,13 +144,14 @@ def main(lims, args):
     PS.remove_arts()
     PS.rout()
 
-    abstract = 'Probably no errors. Maybe passed '+str(len(PS.rerun_samples.keys()))+' arts for rerun.'
-    #if PS.warning_duplicate_samples......
-
-    #if PS.remove_from_WF:
-    #    abstract += ' Probably removed ' + str(len(PS.remove_from_WF))+ ' artifacts from the wf.'
-
-    print >> sys.stderr, abstract ## How do "flush this message in a correct way?"
+    abstract = ''
+    if PS.arts_to_remove:
+        abstract += ' Removed ' + str(len(PS.arts_to_remove))+ ' artifacts from the wf.'
+    if PS.rerun_arts:
+        abstract += str(len(PS.rerun_arts)) + ' artifacts were sent for rerun: '+', '.join(PS.rerun_arts.keys())+'.'
+    if PS.warning_duplicate_samples:
+        abstract += 'WARNING: some samples were sent for rerun as part of more than one pool'
+    print >> sys.stderr, abstract
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESC)
