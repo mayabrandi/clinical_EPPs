9d83de9430128ac8e46e5ab7b4f7660c5987ce14
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2018-10-19 11:21:00 +0200

    updated to handle multipme plates

--- EPPs/make_MAF_plate_layout.py
+++ EPPs/make_MAF_plate_layout.py
@@ -21,30 +21,36 @@ Written by Maya Brandi, Clinical Genomics, Stockholm, Sweden"""
 class PlateSetup():
     def __init__(self, process, MAF_file):
         self.process = process
-        out_analytes = [a for a in process.all_outputs() if a.type=='Analyte']
+        self.out_analytes = [a for a in process.all_outputs() if a.type=='Analyte']
         self.rows = map(chr, range(97, 105))
         self.columns = range(1,13)
-        self.art_dict = {a.location[1].lower() :  a for a in out_analytes}
-        self.plate_name = out_analytes[0].container.name
+        self.container_names = list(set([a.container.name for a in self.out_analytes]))
+        self.art_dict = {key: {} for key in self.container_names}
         self.MAF_file = MAF_file + '_Plate_layout.xls'
         self.headers = []
         self.data = {}
 
-    def make_xls(self):
-        for col in self.columns:
-            for row in self.rows:
-                self.data['Plate'].append(1)
-                self.data['Row'].append(row)
-                self.data['Column'].append(int(col))
-                location = row + ':' + str(col)
-                self.data['Project Code'].append(self.plate_name)
-                if location in self.art_dict:
-                    sample = self.art_dict[location].samples[0]
-                    self.data['Sample ID NO'].append('CG-'+ sample.id)
-                else:
-                    self.data['Sample ID NO'].append('')                    
+    def prepare_plate_sample_info(self):
+        for art in self.out_analytes:
+            self.art_dict[art.container.name][art.location[1].lower()] = art
 
-        df = pd.DataFrame(self.data) 
+    def make_xls(self):
+        self.container_names.sort()
+        for i , container_name in enumerate(self.container_names):
+            for col in self.columns:
+                for row in self.rows:
+                    self.data['Plate'].append(i+1)
+                    self.data['Row'].append(row)
+                    self.data['Column'].append(int(col))
+                    location = row + ':' + str(col)
+                    self.data['Project Code'].append(container_name)
+                    if location in self.art_dict[container_name]:
+                        sample = self.art_dict[container_name][location].samples[0]
+                        self.data['Sample ID NO'].append('CG-'+ sample.id)
+                    else:
+                        self.data['Sample ID NO'].append('')
+
+        df = pd.DataFrame(self.data)
         df = df[self.headers]
         writer = pd.ExcelWriter(self.MAF_file, engine='xlsxwriter')
         workbook  = writer.book
@@ -54,7 +60,7 @@ class PlateSetup():
         worksheet = writer.sheets['Sheet1']
         worksheet.set_column('A:B', 18)
         worksheet.set_column('C:E', 8, format)
-        
+
         writer.save()
 
 
@@ -68,10 +74,11 @@ class PlateSetup():
 def main(lims, args):
     process = Process(lims, id = args.pid)
     PS = PlateSetup(process, args.MAF_file)
+    PS.prepare_plate_sample_info()
     PS.set_header_section()
     PS.make_xls()
     abstract = ''
-    
+
     if abstract:
         logging.warning(abstract)
         sys.exit(abstract)
@@ -86,8 +93,8 @@ if __name__ == "__main__":
                         help=('File name for standard log file, '
                               'for runtime information and problems.'))
     parser.add_argument('-f', dest = 'MAF_file',
-                        help=('File path to new Plate Layout file'))    
-                        
+                        help=('File path to new Plate Layout file'))
+
     args = parser.parse_args()
     lims = Lims(BASEURI, USERNAME, PASSWORD)
     lims.check_version()
