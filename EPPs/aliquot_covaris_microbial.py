2efb5cf2335a774d3c861e29a8a51a13236d5720
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2019-03-26 11:15:21 +0100

    uptated thresholds

--- EPPs/aliquot_covaris_microbial.py
+++ EPPs/aliquot_covaris_microbial.py
@@ -9,9 +9,6 @@ from genologics.lims import Lims
 from genologics.config import BASEURI,USERNAME,PASSWORD
 
 from genologics.entities import Process
-from genologics.epp import EppLogger
-
-import logging
 import sys
 
 class BufferVolume():
@@ -21,41 +18,34 @@ class BufferVolume():
         self.passed_arts = 0
         self.failed_arts = 0
         self.missing_udfs = False
-        self.high_total_volume = []
+        self.buffer_out_of_range = []
         self.final_concentration = float(process.udf['Final Concentration (ng/ul)'])
 
     def get_artifacts(self):
         all_artifacts = self.process.all_outputs(unique=True)
         self.artifacts = filter(lambda a: a.output_type == "Analyte", all_artifacts)
 
-    def check_udfs(self, artifact):
-        try:
-            #amount_needed = float(artifact.udf['Amount needed (ng)'])
-            concentration = float(artifact.udf['Concentration'])
-        except:
-            #amount_needed = None
-            self.missing_udfs = True
-        return concentration
-
     def apply_calculations(self):
         for artifact in self.artifacts:
-            concentration = self.check_udfs(artifact)
-            samp_vol = 4
-            artifact.udf['Sample Volume (ul)'] = samp_vol
-            if concentration <= self.final_concentration:
-                artifact.udf['Volume Buffer (ul)'] = 0
-                self.passed_arts +=1
-                artifact.udf['Total Volume (uL)'] = samp_vol
-            elif concentration > self.final_concentration:
-                total_volume = (concentration*samp_vol)/self.final_concentration
-                eb_volume = total_volume - samp_vol
-                artifact.udf['Volume Buffer (ul)'] = eb_volume
-                self.passed_arts +=1
-                artifact.udf['Total Volume (uL)'] = total_volume
-                if eb_volume > 180:
-                    self.high_total_volume.append(artifact.samples[0].name) #samples will always be a list of only one value
-            else:
+            concentration = artifact.udf.get('Concentration')
+            if not concentration:
+                self.missing_udfs = True
                 self.failed_arts +=1
+                continue
+            artifact.qc_flag = 'PASSED'
+            if concentration <= self.final_concentration:
+                samp_vol = 15
+                buffer_volume = 0
+            else:
+                samp_vol = 4
+                buffer_volume = float(concentration*samp_vol)/self.final_concentration - samp_vol
+                if not 2 < buffer_volume < 180:
+                    self.buffer_out_of_range.append(artifact.samples[0].name)
+                    artifact.qc_flag = 'FAILED'
+            self.passed_arts +=1
+            artifact.udf['Total Volume (uL)'] = buffer_volume + samp_vol
+            artifact.udf['Volume Buffer (ul)'] = buffer_volume
+            artifact.udf['Sample Volume (ul)'] = samp_vol
             artifact.put()
 
 def main(lims,args):
@@ -74,8 +64,8 @@ def main(lims,args):
         sys.exit('Could not apply calculations for all samples. "Final Concentration (ng/ul)" and "Concentration" must be set.')
     elif BV.failed_arts:
         sys.exit(abstract)
-    elif BV.high_total_volume:
-        abstract = "Samples: " + ', '.join(BV.high_total_volume) + ", had over 180 ul total volume. " + abstract
+    elif BV.buffer_out_of_range:
+        abstract = abstract+" Samples: " + ', '.join(BV.buffer_out_of_range) + ", got a high or low Buffer Volume."
         sys.exit(abstract)
     else:
         print >> sys.stderr, abstract
@@ -85,8 +75,6 @@ if __name__ == "__main__":
     parser = ArgumentParser(description=DESC)
     parser.add_argument('--pid',
                         help='Lims id for current Process')
-    parser.add_argument('--log',
-                        help='Log file for runtime info and errors.')
     args = parser.parse_args()
 
     lims = Lims(BASEURI, USERNAME, PASSWORD)
