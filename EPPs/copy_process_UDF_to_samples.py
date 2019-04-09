de4d048e9a51e32d66df7835d9014ee29eaa8d8e
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2019-04-09 11:27:20 +0200

    in respons to review with Isak

--- EPPs/copy_process_UDF_to_samples.py
+++ EPPs/copy_process_UDF_to_samples.py
@@ -13,17 +13,24 @@ Written by Maya Brandi, Science for Life Laboratory, Stockholm, Sweden
 """
 
 
-class CopyUdfs():
+class CopyUDF():
 
     def __init__(self, process):
         self.process = process
-        self.date = process.udf.get('Date delivered')
+        self.date = self._get_date()
         self.samples = []
+
+    def _get_date(self):
+        date = process.udf.get('Date delivered')
+        if not date:
+            sys.exit('Date delivered is not set.')
+        return date
 
     def get_samples(self):
         all_artifacts = self.process.all_inputs()
-        for art in all_artifacts: self.samples += art.samples
-        self.samples = list(set(self.samples))
+        for art in all_artifacts: 
+            self.samples += art.samples
+        self.samples = set(self.samples)
 
     def set_date(self):
         for sample in self.samples:
@@ -31,13 +38,14 @@ class CopyUdfs():
             sample.put()
 
 def main(lims, args):
+    lims = Lims(BASEURI, USERNAME, PASSWORD)
+    lims.check_version()
+
     process = Process(lims, id = args.pid)
-    CUdf = CopyUdfs(process)
-    
-    if not CUdf.date:
-        sys.exit('Date delivered is not set.')
-    CUdf.get_samples()
-    CUdf.set_date()
+
+    CUDF = CopyUDF(process)
+    CUDF.get_samples()
+    CUDF.set_date()
 
 
 if __name__ == "__main__":
@@ -45,6 +53,4 @@ if __name__ == "__main__":
     parser.add_argument('--pid',
                         help='Lims id for current Process')
     args = parser.parse_args()
-    lims = Lims(BASEURI, USERNAME, PASSWORD)
-    lims.check_version()
-    main(lims, args)
+    main(args)
