3f0faf3a0a7e0b31739c30afc31e080340c62f55
Maya <maya.brandi@scilifelab.se>
2019-04-30 07:57:25 +0200

    remove hard coded

--- EPPs/place_samples_in_seq_agg.py
+++ EPPs/place_samples_in_seq_agg.py
@@ -21,6 +21,7 @@ api.setVersion( VERSION )
 api.setup( USERNAME, PASSWORD )
 
 
+
 DESC = """
 Script to pass samples and pools to next step. Sort of specific script:
 
@@ -41,14 +42,15 @@ Written by Maya Brandi, Science for Life Laboratory, Stockholm, Sweden
 
 class PassSamples():
 
-    def __init__(self, process):
+    def __init__(self, process, process_types, seq_agr_step):
         self.process = process
+        self.process_types = process_types
         self.input_arts = self.process.all_inputs(unique=True)
         self.samples = []
         self.remove_from_WF = []
         self.send_to_next_step = []
         self.all_arts_in_sort = []
-        self.next_step = ['CG002 - Sequence Aggregation']
+        self.next_step = [seq_agr_step]
         self.current_WF = ''
         self.next_step_stage = ''
         self.rXML = []
@@ -84,7 +86,7 @@ class PassSamples():
 
     def _get_pools_from_sort(self, sample): ##---> will give manny duplicates
         self.all_arts_in_sort += lims.get_artifacts(samplelimsid = sample.id,
-                            process_type = ["CG002 - Sort HiSeq Samples", "CG002 - Sort HiSeq X Samples (HiSeq X)"])
+                            process_type = self.process_types)
 
     def _make_unique_pools(self):
         # Make uniqe. Also esure there are no replicates of the same RML. 
@@ -147,7 +149,7 @@ class PassSamples():
 
 def main(lims, args):
     process = Process(lims, id = args.pid)
-    PS = PassSamples(process)
+    PS = PassSamples(process, args.process_types, args.seq_agr_step)
     PS.get_samples()
     PS.get_artifacts()
     PS.get_current_WF()
@@ -165,6 +167,11 @@ if __name__ == "__main__":
     parser = ArgumentParser(description=DESC)
     parser.add_argument('--pid',
                         help='Lims id for current Process')
+    parser.add_argument('-p', dest = 'process_types',  nargs='+', 
+                        help='Get pools from these process type(s)')
+    parser.add_argument('-s', dest = 'seq_agr_step', 
+                        help='Get place samples in this step)')
+                
     args = parser.parse_args()
 
     lims = Lims(BASEURI, USERNAME, PASSWORD)
