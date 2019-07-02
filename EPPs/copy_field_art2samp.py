7d5a58b44fda1c3f870513596fdff217b3c7e91a
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2019-07-02 09:24:08 +0200

    responce to Isaks komments

--- EPPs/copy_field_art2samp.py
+++ EPPs/copy_field_art2samp.py
@@ -16,39 +16,38 @@ DESC = """
 
 
 class CopyUDF():
-    def __init__(self, process, samp_udf, art_udf):
-        self.process = process
+    def __init__(self, process, sample_udf, art_udf):
         self.artifacts = [a for a in process.all_outputs() if a.type=='Analyte']
-        self.samp_udf = samp_udf
+        self.sample_udf = sample_udf
         self.art_udf = art_udf
-        self.failed_udfs = False
+        self.failed_udfs = 0
 
     def copy(self):
         for art in self.artifacts:
             sample = art.samples[0]
             udf = art.udf.get(self.art_udf)
             if udf is not None:
-                sample.udf[self.samp_udf] = udf
+                sample.udf[self.sample_udf] = udf
                 sample.put()
             else:
-                self.failed_udfs = True
+                self.failed_udfs += 1
 
 def main(lims, args):
     process = Process(lims, id = args.pid)
-    CUDF = CopyUDF(process, args.samp_udf, args.art_udf)
+    CUDF = CopyUDF(process, args.sample_udf, args.art_udf)
     CUDF.copy()
 
     if CUDF.failed_udfs:
-        sys.exit('failed to copy some udfs')
+        sys.exit('Failed to copy udf for %s samples.' % (str(CUDF.failed_udfs)) )
     else:
-        print >> sys.stderr, 'UDFs were succsessfully copied!'
+        print >> sys.stderr, 'UDFs were successfully copied!'
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESC)
     parser.add_argument('-p', dest = 'pid',
                         help='Lims id for current Process')
-    parser.add_argument('-s', dest = 'samp_udf', help=(''))
+    parser.add_argument('-s', dest = 'sample_udf', help=(''))
     parser.add_argument('-a', dest = 'art_udf',
                         help=(''))
     args = parser.parse_args()
