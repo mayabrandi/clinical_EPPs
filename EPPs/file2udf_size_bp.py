b5d9a66e5fedc48625277c7562c6aa0f2f5b0eed
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2019-10-31 16:46:09 +0100

    adding doc strings

--- EPPs/file2udf_size_bp.py
+++ EPPs/file2udf_size_bp.py
@@ -12,11 +12,7 @@ import os
 
 
 DESC = """epp script to ...
-    Reads a excel file with column names: Well, col_name 1, col_name 2, etc.
-
-    On the artifact located in the well, sets the value from col_name 1 on udf 1, col_name on udf 2, etc. 
-    Not in prod yet. 
-
+    Reads a csv. Fetches the columns 'WellId' and 'Average Size [bp]'. And sets the aritfact udfs, based on well location.
 
 Written by Maya Brandi, Science for Life Laboratory, Stockholm, Sweden
 """
@@ -35,6 +31,9 @@ class File2UDF():
         self.result_file = None
 
     def get_artifacts(self):
+        """Gets input and output artifacts from input_output_maps.
+        Gets the location from the inart, but saves the outart in the artifacts dictionary."""
+
         for inp, outp in self.input_output_maps:
             if outp.get("output-generation-type") == "PerAllInputs":
                 continue
@@ -46,6 +45,9 @@ class File2UDF():
             self.all_arts += 1
 
     def get_result_file(self, result_file):
+        """Reads file from args if present. Otherwise searches for file in outarts with 
+        name Tapestation CSV"""
+
         if result_file and os.path.isfile(result_file):
             self.result_file = result_file
         else:
@@ -56,6 +58,8 @@ class File2UDF():
                 self.result_file = files[0].files[0].content_location.split('scilifelab.se')[1]
 
     def set_udfs(self):
+        """Reads the csv and sets the average size bp for each sample"""
+
         with open(self.result_file) as f:
             d = DictReader(f, delimiter=';')
             l = list(d)
