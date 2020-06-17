9ebfc65a36b2300f92dbf8a1e0b9c587e6d0bc21
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2020-06-17 11:58:05 +0200

    separating pooling calculation from other calculations

--- EPPs/pool_calculations_twist.py
+++ EPPs/pool_calculations_twist.py
@@ -13,16 +13,17 @@ from genologics.config import BASEURI,USERNAME,PASSWORD
 from genologics.entities import Process
 import sys
 
-class Pool(self):
+class Pool:
     def __init__(self, pool):
         self.qc_flag='PASSED'
         self.total_volume = 0
         self.total_reads = 0
+        self.pool = pool
         self.pool_size = pool.udf.get('Total Amount (ng)')
         self.artifacts = pool.input_artifact_list()
 
-
     def get_total_reads(self):
+        """Get the total numer of missing reads in the pool"""
         for art in self.artifacts:
             reads = art.samples[0].udf.get('Reads missing (M)')
             concentration = art.udf.get('Concentration')
@@ -33,14 +34,14 @@ class Pool(self):
             sys.exit('All samples seem to have Missing Reads = 0. You dont want to sequence any of the samples?')
 
     def calculate_amount_and_volume(self):
-        for art in artifacts:
+        for art in self.artifacts:
             reads = art.samples[0].udf.get('Reads missing (M)')
             concentration = art.udf.get('Concentration')
             fract_of_pool = reads/float(self.total_reads)
             amount_to_pool = self.pool_size * fract_of_pool
             vol = amount_to_pool/concentration
-            if amount_to_pool > art.udf.get('Amount (ng)'):
-                pool.qc_flag='FAILED'
+            if amount_to_pool > art.udf.get('Amount (ng)') or amount_to_pool < 187.5:
+                self.pool.qc_flag='FAILED'
                 self.amount_fail = True
             art.udf['Amount taken (ng)'] = amount_to_pool
             art.udf['Volume of sample (ul)'] = vol
@@ -48,27 +49,21 @@ class Pool(self):
             self.total_volume += vol
 
 
-class CalculationsTwist:
-    def __init__(self, process):
-        self.process = process
-        self.iom = process.input_output_maps
-        self.artifacts = []
-        self.amount_fail = False
+class CalculationsForPools:
+    def __init__(self, process, pools):
+        self.pools = pools
         self.okej = 0
         self.failed = 0
-        self.missing_udfs = []
-        self.low_conc = 0
         self.all_volumes= []
-
-    def get_artifacts(self, process_type):
-        all_artifacts = self.process.all_outputs(unique=True)
-        self.artifacts = filter(lambda a: a.output_type == "Analyte" , all_artifacts)
+        self.amount_fail = False
 
     def calculate_volumes_for_pooling(self):
-        for pool_art in self.artifacts:
-            pool = Pool(art)
+        for pool_art in self.pools:
+            pool = Pool(pool_art)
             pool.get_total_reads()
             pool.calculate_amount_and_volume()
+            if pool.amount_fail:
+                self.amount_fail = True
             if pool.total_volume:
                 pool_art.udf['Total Volume (ul)'] = pool.total_volume
                 self.all_volumes.append(pool.total_volume)
@@ -77,27 +72,30 @@ class CalculationsTwist:
                 self.failed +=1
 
     def calculate_volume_wather(self):
-        for pool in self.artifacts:
-            pool.udf['Volume H2O (ul)'] = max(self.all_volumes) - pool.udf.get('Total Volume (ul)')
-            pool.put()
+        for pool_art in self.pools:
+            if pool_art.udf.get('Total Volume (ul)'):
+                pool_art.udf['Volume H2O (ul)'] = max(self.all_volumes) - pool_art.udf.get('Total Volume (ul)')
+            pool_art.put()
             
 
 def main(lims,args):
     process = Process(lims, id = args.pid)
-    AT = CalculationsTwist(process)
-    AT.get_artifacts(args.calculate)
-    AT.calculate_volumes_for_pooling()
-    AT.calculate_volume_wather() 
-    abstract = ''
-    if AT.amount_fail:
-        abstract += 'Input amount low for samples in pool. Generate placement map for more info. '
-    if AT.failed:
-        missing = ', '.join( list(set(AT.missing_udfs)))
-        abstract += 'Failed to perform calculations for '+ str(AT.failed)+ ' samples. Some of the following udfs are invalid or missing: ' +   missing + '. '
-    if AT.okej:
-        abstract += 'Performed calculations for '+ str(AT.okej)+ ' samples.'
+    pools = filter(lambda a: a.output_type == "Analyte" , process.all_outputs(unique=True))
+    CFP = CalculationsForPools(process, pools)
+    CFP.calculate_volumes_for_pooling()
+    CFP.calculate_volume_wather()
 
-    if AT.failed or AT.amount_fail:
+ 
+    abstract = ''
+    if CFP.amount_fail:
+        abstract += 'Input amount low for samples in pool. Generate placement map for more info. '
+    if CFP.failed:
+        missing = ', '.join( list(set(CFP.missing_udfs)))
+        abstract += 'Failed to perform calculations for '+ str(CFP.failed)+ ' pools. Some of the following udfs are invalid or missing: ' +   missing + '. '
+    if CFP.okej:
+        abstract += 'Performed calculations for '+ str(CFP.okej)+ ' pools.'
+
+    if CFP.failed or CFP.amount_fail:
         sys.exit(abstract)
     else:
         print >> sys.stderr, abstract
